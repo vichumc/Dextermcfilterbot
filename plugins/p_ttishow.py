fda4d4cd0d94a6fdc6c6c4ff8fdbfa8351243ba2
vichumc <100103080+vichumc@users.noreply.github.com>
2022-04-14 14:00:14 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -37,8 +37,8 @@ async def save_group(bot, message):
             await bot.leave_chat(message.chat.id)
             return
         buttons = [[
-            InlineKeyboardButton('👀 Help 👀', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('⚡ Updates channel ⚡', url='https://t.me/moviesclubupdates')
+            InlineKeyboardButton('Help 👀', url=f"https://t.me/{temp.U_NAME}?start=help"),
+            InlineKeyboardButton('Updates channel ⚡', url='https://t.me/moviesclubupdates')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
