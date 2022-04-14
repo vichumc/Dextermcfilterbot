2627fe6b4808f9b3e1d7419f23920afcd8c342aa
vichumc <100103080+vichumc@users.noreply.github.com>
2022-04-14 13:23:14 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,7 +1,8 @@
 class script(object):
-    START_TXT = """𝙷𝙴𝙻𝙾 {},
-🥰 𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>,I AM A MOVIE BOT!!
-JUST ADD ME TO YOUR GROUPS & REQUEST MOVIES 🤗🎬"""
+    START_TXT = """Hy {},
+My name is <a href=https://t.me/{}>{}</a>,
+I AM A MOVIE BOT!!
+Just add me to your groups and request Movies/Series 😍🎬
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """🐶 𝙼𝚈 𝙽𝙰𝙼𝙴: {} 
