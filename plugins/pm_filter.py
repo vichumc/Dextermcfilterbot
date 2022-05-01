688ad00fa12bfa057e103beef0eee88d2b872952
vichumc <100103080+vichumc@users.noreply.github.com>
2022-05-01 22:46:06 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -369,7 +369,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     elif query.data.startswith("checksub"):
         if AUTH_CHANNEL and not await is_subscribed(client, query):
-            await query.answer("I Like Your Smartness, But Don't Be Oversmart 🥱",show_alert=True)
+            await query.answer("FIRST JOIN UPDATES CHANNEL‼️ Then Press Try Again 🔃",show_alert=True)
             return
         ident, file_id = query.data.split("#")
         files_ = await get_file_details(file_id)
