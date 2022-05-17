a15c6cab937fee6b60bd52734d44a608c09d1042
vichumc <100103080+vichumc@users.noreply.github.com>
2022-05-17 23:45:27 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -359,7 +359,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     file_id=file_id,
                     caption=f_caption
                     )
-                await query.answer('📌 Check my msg, I have sent the Movie!',show_alert = True)
+                await query.answer('📌 Check my personal message, \nI have sent the Movie!',show_alert = True)
         except UserIsBlocked:
             await query.answer('Unblock me First!😼',show_alert = True)
         except PeerIdInvalid:
