b2beedb0303b6946ee27f82147e0dad8adfa903f
vichumc <100103080+vichumc@users.noreply.github.com>
2022-07-07 12:05:26 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -61,7 +61,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"{get_size(file.file_size)}{file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"{get_size(file.📁file_size)}{file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
@@ -70,10 +70,10 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"{file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"{file.📁file_name}", callback_data=f'files#{file.file_id}'
                 ),
                 InlineKeyboardButton(
-                    text=f"{get_size(file.file_size)}",
+                    text=f"{get_size(file.📁file_size)}",
                     callback_data=f'files_#{file.file_id}',
                 ),
             ]
