9eb69cc0d6d4bbfe48367c6dd2e8636b6865e62a
Joel Bx <87010768+Cinemavillaz@users.noreply.github.com>
2022-08-01 12:58:31 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -642,7 +642,10 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"Here is what i found for your query {search}"
+        cap = f"""**📁 Here is What I Found In My Database For Your Query : #{query} 👇**
+
+
+<code>➲ ᴛʜɪs ᴍᴇssᴀɢᴇ ᴡɪʟʟ ʙᴇ ᴅᴇʟᴇᴛɪɴɢ ɪɴ 5 ᴍɪɴᴜᴛᴇ</code>"""
     if imdb and imdb.get('poster'):
         try:
             msg = await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
