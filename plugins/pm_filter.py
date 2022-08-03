68c736f865f49b812b5ecd98c97b4892b3887115
vichumc <100103080+vichumc@users.noreply.github.com>
2022-08-03 16:41:44 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -708,9 +708,9 @@ async def advantage_spell_chok(msg):
                 )
             ] for k, movie in enumerate(movielist)]
     btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
-    await msg.reply("SPELLING NOT CORRECT‼️ \nCLICK CORRECT MOVIE NAME FROM BELOW 👇", reply_markup=InlineKeyboardMarkup(btn))
+    k = await msg.reply("SPELLING NOT CORRECT‼️ \nCLICK CORRECT MOVIE NAME FROM BELOW 👇", reply_markup=InlineKeyboardMarkup(btn))
     await asyncio.sleep(10)
-    await reply.delete(5)
+    await k.delete(5)
 
 async def manual_filters(client, message, text=False):
     group_id = message.chat.id
